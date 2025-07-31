68a98ec16269a89e3c2ce12f475fec9c7780a158
Alan <alanmathew190@gmail.com>
2025-07-31 15:54:51 +0530

    first

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("hello");
+// document.writeln("hello world");
+
+var a = 10;
+console.log(a);
+
+let name = "Alan"
+let age = 22
+let place="kundara"
+let isOnline = true
+
+console.log(typeof age)
+console.log(name)
+var z;
+var c = 11;
+let b = 20;
+
+console.log(z)
+console.log(a);
+console.log(a + b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a % b)
+console.log(a ** b)
+// console.log(c++)
+c=c+5
+console.log(c)
+c = c - 6;
+console.log(c)
+
+let d = 5;
+let e = "5"
+console.log(d == e)
+console.log(d === e)
+console.log(d != e)
+console.log(d !== e)
+
+console.log(d && e)
+console.log(d || e)
+console.log(!d)
+console.log(!e)
+
+
+const one = {
+    name: "Alan",
+    age: 21,
+    place: "kundara"
+}
+
+console.log(one)
+console.log(one.name + " is " + one.age + " years old and lives in " + one.place)
+document.write(one.name + " is " + one.age + " years old and lives in " + one.place )
+// document.getElementById("demo").innerHTML = "hello world";
+
+  
+
