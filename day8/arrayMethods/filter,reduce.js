9a656f9337d2cd88468428167db31a089a88da02
Alan <alanmathew190@gmail.com>
2025-08-14 13:32:14 +0530

    commit

--- day8/arrayMethods/filter,reduce.js
+++ day8/arrayMethods/filter,reduce.js
@@ -3,3 +3,8 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const newArray1 = numbers.filter(n => n % 2 == 0)
 console.log(newArray1)
+
+//reduce
+
+const sum = numbers.reduce((acc, num) => acc + num, 0)
+console.log(sum)
