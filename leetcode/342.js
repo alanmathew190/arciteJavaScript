73ebe53103b848a34cd0e73a821c2f8d4c139625
Alan <alanmathew190@gmail.com>
2025-08-26 22:07:33 +0530

    push

---
+++ leetcode/342.js
@@ -0,0 +1,15 @@
+// var isPowerOfFour = function (n) {
+//     if (n <= 0) return false;
+//     return (Math.log(n) / Math.log(4)) % 1 === 0;
+
+// };
+
+// console.log(isPowerOfFour(15))
+
+function isPowerOfFour(n) {
+  for (let i = 0; Math.pow(4, i) <= n; i++) {
+    if (Math.pow(4, i) === n) return true;
+  }
+  return false;
+}
+console.log(isPowerOfFour(16));
